cd5c252185711c82a3277d280d4fdb17e3f6bc58
TManjusaka <wuskipara@gmail.com>
2015-11-28 12:13:42 -0800

    new heuristics

---
+++ Hashing.py
@@ -0,0 +1,49 @@
+import cPickle as pickle
+
+def Hashing():
+    k = 8       ##number of machines
+    filename = 'com-dblp.ungraph'
+    order = 'bfs'
+
+    adList = pickle.load(open(filename+'.p','rb'))
+    Stream = pickle.load(open(filename+'_'+order+'.p','rb'))
+    print type(adList),type(Stream)
+    print adList[3],adList[35]
+    print "total nodes:",len(adList.keys()),len(Stream)
+
+    print "Partition"
+
+    Cap = len(Stream)/k + 1 ##Capacity of each machine
+    curSelectedMach = 0     ##init the default selected machine number
+    cutEdges = 0            ##number of edges being cut
+    Machines = []           ##List of machines
+    Machine = set()         ##Single machine
+    for i in xrange(k):             ##init k sets
+        Machine = set()         ##Single machine
+        Machines.append(Machine)
+
+
+    for curNode in Stream:
+        ##Use hash to find machine
+        ##TODO HASH FUN need to change
+        curSelectedMach = curNode % k
+        Machines[curSelectedMach].add(curNode)
+
+        #print curSelectedMach
+        ##Calculate cutEdges
+        curNeighbors = adList[curNode]
+        del adList[curNode]
+        for Neighbor in curNeighbors:
+            if Neighbor == curNode:          ##Skip first node
+                continue
+            for i in xrange(k):
+                if Neighbor in Machines[i] and i != curSelectedMach:     ##Neighbor was in a machine but not same as curNode
+                    cutEdges += 1
+                else:
+                    continue
+
+    print cutEdges
+
+
+if __name__ == '__main__':
+    Hashing()
