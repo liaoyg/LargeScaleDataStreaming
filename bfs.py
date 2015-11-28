f64cf0b949df17a36102ed4bcd8f6656b0017a84
TManjusaka <wuskipara@gmail.com>
2015-11-28 14:34:27 -0800

    modified

--- bfs.py
+++ bfs.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import cPickle as pickle
 from collections import deque
 
@@ -39,45 +38,3 @@ def bfs():
 
 if __name__ == '__main__':
     bfs()
-=======
-import cPickle as pickle
-from collections import deque
-
-def bfs():
-    filename = 'com-dblp.ungraph'
-
-    adList = pickle.load(open(filename+'.p','rb'))
-    print type(adList)#,adlis.readline()
-    print adList[3],adList[35]
-    print "total nodes:",len(adList.keys())
-
-    print "bfsing"
-    #bfs
-    q = deque()         #init
-    curNode = 0         #first node
-    q.append(curNode)
-    bfsOrder = []       #result
-    visited = set()
-    while q:
-        curNode = q.popleft()           ##dequeue
-        if curNode not in visited:
-            bfsOrder.append(curNode)    ##output to bfsOrder
-            visited.add(curNode)
-        else:
-            pass
-        if curNode not in adList:       ##may have no neighbors   NOT NECESSARY
-            pass
-        else:
-            curNeighbor = adList[curNode]   ##all neighbors
-            del adList[curNode]             ##delete from adList
-            for i in curNeighbor:           ##enqueue all neighbors
-                if i in adList:
-                    q.append(i)
-
-    print len(bfsOrder),len(visited)    ##test
-    pickle.dump(bfsOrder,open(filename+'_bfs'+'.p','wb'))
-
-
-if __name__ == '__main__':
-    bfs()
->>>>>>> origin/master
