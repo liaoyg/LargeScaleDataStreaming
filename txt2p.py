4f68e516ae4586ced499a6e7a370d32456ce9401
liaoyg <liaoyg620@163.com>
2015-11-28 12:02:39 -0800

    v1.0

--- txt2p.py
+++ txt2p.py
@@ -1,15 +1,14 @@
-import numpy as np
 import cPickle as pickle
 from collections import deque
 
-def main():
+def txt2p():
     filename = 'com-dblp.ungraph'
     #open file
     fr = open((filename+".txt"))
 
     offset = 4
     
-    #total number of examples
+    #total number of edges
     line_num = len(fr.readlines())- offset 
     print line_num
 
@@ -18,68 +17,39 @@ def main():
     for i in range(offset):
         header = fr.readline()
     
-
-    node = []
-    adlist = dict()
-    i=0
+    print "loading"
+    adList = dict()
     total_node = set()
     for line in fr.readlines():
         edge = line.strip().split()
-        if edge[0] not in total_node:
+        if edge[0] not in total_node:   ##first appearance
             total_node.add(edge[0])
-        if edge[1] not in total_node:
+            key = int(edge[0])
+            adList[key] = []
+            adList[key].append(int(edge[0]))
+            adList[key].append(int(edge[1]))
+        else:
+            key = int(edge[0])
+            adList[key].append(int(edge[1]))  
+
+        if edge[1] not in total_node:   ##first appearance
             total_node.add(edge[1])
-        if node == []:
-            node.append(int(edge[0]))
-            node.append(int(edge[1]))
-        elif int(edge[0]) == node[0]:
-            node.append(int(edge[1]))
+            key = int(edge[1])
+            adList[key] = []
+            adList[key].append(int(edge[1]))
+            adList[key].append(int(edge[0]))
         else:
-            #adlist.add(node)
-            #print node
-            key = (node[0])
-            adlist[key] = node
-            node = []
-            node.append(int(edge[0]))
-            node.append(int(edge[1]))
-            i+=1
+            key = int(edge[1])
+            adList[key].append(int(edge[0]))  
+
+
     edges = 0
-    for j in adlist.keys():
-        edges += len(adlist[j])-1
-    print "total nodes:", i,len(total_node),len(adlist.keys())
+    for j in adList.keys():
+        edges += (len(adList[j])-1)
+    print "total nodes:", len(total_node),len(adList.keys())
     print "total edges:",edges
-    pickle.dump(adlist,open(filename+'.p','wb'))
-
-    adlist = pickle.load(open(filename+'.p','rb'))
-    print type(adlist)#,adlis.readline()
-    print adlist[3],adlist[35]
-    print len(adlist.keys())
-
-
-    #bfs
-    q = deque()
-    curNode = 0
-    q.append(curNode)
-    bfsQueue = []
-    visited = set()
-    while q:
-        curNode = q.popleft()       ##dequeue
-        if curNode not in visited:
-            bfsQueue.append(curNode)    ##enqueue
-            visited.add(curNode)
-        else:
-            pass
-        if curNode not in adlist:   ##may have no neighbors
-            pass
-        else:
-            curList = adlist[curNode]   ##all neighbors
-            del adlist[curNode]         
-            for i in curList:
-                if i in adlist:
-                    q.append(i)
-
-    print len(bfsQueue),len(visited)#,bfsQueue
+    pickle.dump(adList,open(filename+'.p','wb'))
     
 
 if __name__ == '__main__':
-    main()
+    txt2p()
