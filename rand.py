5536c9faec0b3c1e2df7bedf04bae925c768cdf1
TManjusaka <wuskipara@gmail.com>
2015-11-28 18:11:37 -0800

    Random order

---
+++ rand.py
@@ -0,0 +1,22 @@
+import cPickle as pickle
+from random import *
+def Rand():
+    filename = 'com-dblp.ungraph'
+
+    adList = pickle.load(open(filename+'.p','rb'))
+    print type(adList)#,adlis.readline()
+    print adList[3],adList[35]
+    print "total nodes:",len(adList.keys())
+
+    print "Random"
+    #random
+    randOrder = []       #result
+    randOrder = adList.keys()
+    shuffle(randOrder)
+    print len(randOrder)    ##test
+    print randOrder[0:10]
+    pickle.dump(randOrder,open(filename+'_rand'+'.p','wb'))
+
+
+if __name__ == '__main__':
+    Rand()
